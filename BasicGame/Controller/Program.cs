2dcc214ee331d15c9c026fe97cdb7fa3e121d9b1
Sharp <rsha7868@740s-pl-127.local>
2018-05-01 12:30:45 -0600

    saving the  animation and the basic game errors.

--- BasicGame/Controller/Program.cs
+++ BasicGame/Controller/Program.cs
@@ -1,4 +1,4 @@
-﻿#region Using Statements
+#region Using Statements
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,11 +20,11 @@ namespace BasicGame
 	static class Program
 #endif
 	{
-		private static Game1 game;
+		private static BasicGame game;
 
 		internal static void RunGame()
 		{
-			game = new Game1();
+			game = new BasicGame();
 			game.Run();
 #if !__IOS__ && !__TVOS__
 			game.Dispose();
---
+++ BasicGame/View/Animation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BasicGame
+{
+	public class Animation
+	{
+		public Animation()
+		{
+		}
+	}
+}
